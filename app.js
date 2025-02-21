7fc2741a7f872f58f49e7e45e63eb75bc803af3f
Thaís Santos <thais_gtape@hotmail.com>
2025-02-20 21:00:25 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,30 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos =[];
+
+    function adicionarAmigo(){
+       let campoDeTexto= document.querySelector('input').value;
+       if (campoDeTexto==''){
+        alert ('Digite um nome válido!')
+      }
+       else {
+        listaDeAmigos.push(campoDeTexto);
+        let mostrarLista= document.getElementById('listaAmigos');
+        mostrarLista.innerHTML=listaDeAmigos;
+    }
+    limpaTela();
+    }
+
+      function sortearAmigo(){
+        let sorteioNomes= Math.floor(Math.random()*listaDeAmigos.length);
+        let result = document.getElementById('resultado');
+         result.innerHTML=  `O amigo secreto sorteado é: ${listaDeAmigos[sorteioNomes]}`;
+      }
+
+      function limpaTela (){
+         campoDeTexto= document.querySelector('input');
+         campoDeTexto.value='';
+      }
+
+
+
